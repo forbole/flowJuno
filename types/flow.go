cc45c8d89af9d1b0d826d4fbed8ef4a406820541
HarleyAppleChoi <appleno56@gmail.com>
2021-08-04 11:47:13 +0100

    Make find event by type

--- types/flow.go
+++ types/flow.go
@@ -5,18 +5,20 @@ import (
 	"time"
 
 	"github.com/onflow/flow-go-sdk"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 )
 
 // Tx represents an already existing blockchain transaction
 type Tx struct{
-	Event []flow.Event
+	Events []flow.Event
 	Status string
 }
 
 // NewTx allows to create a new Tx instance from the given txResponse
 func NewTx(tx flow.TransactionResult)Tx{
 	return Tx{
-		Event:tx.Events,
+		Events:tx.Events,
 		Status:tx.Status.String(),
 	}
 }
@@ -25,20 +27,20 @@ func NewTx(tx flow.TransactionResult)Tx{
 // FindEventByType searches inside the given tx events for the message having the specified index, in order
 // to find the event having the given type, and returns it.
 // If no such event is found, returns an error instead.
-func (tx Tx) FindEventByType(index int, eventType string) (sdk.StringEvent, error) {
-	for _, ev := range tx.Logs[index].Events {
+func (tx Tx) FindEventByType(index int, eventType string) (flow.Event, error) {
+	for _, ev := range tx.Events{
 		if ev.Type == eventType {
 			return ev, nil
 		}
 	}
 
-	return sdk.StringEvent{}, fmt.Errorf("no %s event found inside tx with hash %s", eventType, tx.TxHash)
+	return flow.Event{}, fmt.Errorf("no %s event found inside tx with hash %s", eventType, tx.TxHash)
 }
 
 // FindAttributeByKey searches inside the specified event of the given tx to find the attribute having the given key.
 // If the specified event does not contain a such attribute, returns an error instead.
-func (tx Tx) FindAttributeByKey(event sdk.StringEvent, attrKey string) (string, error) {
-	for _, attr := range event.Attributes {
+func (tx Tx) FindAttributeByKey(event flow.Event, attrKey string) (string, error) {
+	for _, attr := range event.TransactionIndex{
 		if attr.Key == attrKey {
 			return attr.Value, nil
 		}
