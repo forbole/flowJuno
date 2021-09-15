9172371526795147f030414371b9d95281457a61
HarleyAppleChoi <appleno56@gmail.com>
2021-09-15 18:09:44 +0100

    update auth test for delegator account

--- db/postgresql/auth_test.go
+++ db/postgresql/auth_test.go
@@ -121,34 +121,39 @@ func (suite *DbTestSuite) TestSaveLockedAccount() {
 	suite.Require().True(expectedAccountRow.Equal(accountRows[0]))
 }
 
-func (suite *DbTestSuite) TestSaveLockedAccount() {
+func (suite *DbTestSuite) TestSaveDelegatorAccount() {
 	baseAccount := suite.SaveAccount()
 	address := baseAccount[0].Address
-	lockedAddress := flow.HexToAddress("0x2")
+	delegatorAddress := flow.HexToAddress("0x2")
 	balance := uint64(10)
 	unlockLimit := uint64(20)
+	delegatorId:=int64(8411)
+	delegatorNodeId:="2cfab7e9163475282f67186b06ce6eea7fa0687d25dd9c7a84532f2016bc2e5e" 
+	nodeInfo:=types.NewDelegatorNodeInfo(uint32(delegatorId),delegatorNodeId,0,0,0,0,0,0)
+	
+	expectedDelegatorNodeId:=`{"Id":8411,"NodeID":"2cfab7e9163475282f67186b06ce6eea7fa0687d25dd9c7a84532f2016bc2e5e","TokensCommitted":0,"TokensStaked":0,"TokensUnstaking":0,"TokensRewarded":0,"TokensUnstaked":0,"TokensRequestedToUnstake":0}`
 
-	accounts := []types.LockedAccount{
-		types.NewLockedAccount(address.String(), lockedAddress.String(), balance, unlockLimit),
+	accounts := []types.DelegatorAccount{
+		types.NewDelegatorAccount(address.String(),delegatorId, delegatorNodeId, nodeInfo),
 	}
 
 	// ------------------------------
 	// --- Save the data
 	// ------------------------------
 
-	err := suite.database.SaveLockedTokenAccounts(accounts)
+	err := suite.database.SaveDelegatorAccounts(accounts)
 	suite.Require().NoError(err)
 
-	err = suite.database.SaveLockedTokenAccounts(accounts)
+	err = suite.database.SaveDelegatorAccounts(accounts)
 	suite.Require().NoError(err, "double account insertion should not insert and returns no error")
 
 	// ------------------------------
 	// --- Verify the data
 	// ------------------------------
-	expectedAccountRow := dbtypes.NewLockedAccountRow(address.String(), lockedAddress.String(), int(balance), int(unlockLimit))
+	expectedAccountRow := dbtypes.NewdelegatorAccountRow(address.String(), delegatorAddress.String(), int(balance), int(unlockLimit))
 
-	var accountRows []dbtypes.LockedAccountRow
-	err = suite.database.Sqlx.Select(&accountRows, `SELECT * FROM locked_account`)
+	var accountRows []dbtypes.delegatorAccountRow
+	err = suite.database.Sqlx.Select(&accountRows, `SELECT * FROM delegator_account`)
 	suite.Require().NoError(err)
 	suite.Require().Len(accountRows, 1, "account table should contain only one row")
 
