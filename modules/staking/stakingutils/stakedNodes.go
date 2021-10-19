bdd1a0af090aa0018f00e4b83e6b93cb981996fb
HarleyAppleChoi <appleno56@gmail.com>
2021-10-19 14:22:08 +0100

    delete redundent delegator request

--- modules/staking/stakingutils/stakedNodes.go
+++ modules/staking/stakingutils/stakedNodes.go
@@ -20,57 +20,12 @@ func GetDataFromNodeID(nodeInfofromNodeId []types.StakerNodeInfo, block *flow.Bl
 	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
 		Msg("getting staked node infos")
 
-	err := getNodeUnstakingTokens(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeTotalCommitment(nodeInfofromNodeId, block, db, flowClient)
+	err := getNodeTotalCommitment(nodeInfofromNodeId, block, db, flowClient)
 	if err != nil {
 		return err
 	}
 
 	err = getNodeTotalCommitmentWithoutDelegators(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeStakingKey(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeStakedTokens(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeRole(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeRewardedTokens(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeNetworkingKey(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeNetworkingAddress(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeInitialWeight(nodeInfofromNodeId, block, db, flowClient)
-	if err != nil {
-		return err
-	}
-
-	err = getNodeCommittedTokens(nodeInfofromNodeId, block, db, flowClient)
 	if err != nil {
 		return err
 	}
@@ -154,95 +109,3 @@ func getNodeTotalCommitmentWithoutDelegators(nodeInfos []types.StakerNodeInfo, b
 	return db.SaveNodeTotalCommitmentWithoutDelegators(totalStakeArr)
 }
 
-func getNodeStakingKey(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating node unstaking tokens")
-
-	totalStakeArr := make([]types.NodeStakingKey, len(nodeInfos))
-	for i, id := range nodeInfos {
-		stakingKey := id.StakingKey
-
-		totalStakeArr[i] = types.NewNodeStakingKey(id.Id, stakingKey, int64(block.Height))
-	}
-
-	return db.SaveNodeStakingKey(totalStakeArr)
-}
-
-func getNodeStakedTokens(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating node unstaking tokens")
-	totalStakeArr := make([]types.NodeStakedTokens, len(nodeInfos))
-	for i, id := range nodeInfos {
-		totalStakeArr[i] = types.NewNodeStakedTokens(id.Id, id.TokensStaked, int64(block.Height))
-	}
-
-	return db.SaveNodeStakedTokens(totalStakeArr)
-}
-
-func getNodeRole(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating node role")
-
-	totalStakeArr := make([]types.NodeRole, len(nodeInfos))
-	for i, id := range nodeInfos {
-		totalStakeArr[i] = types.NewNodeRole(id.Id, id.Role, int64(block.Height))
-	}
-
-	return db.SaveNodeRole(totalStakeArr)
-}
-
-func getNodeRewardedTokens(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating get node rewarded tokens")
-
-	totalStakeArr := make([]types.NodeRewardedTokens, len(nodeInfos))
-	for i, id := range nodeInfos {
-		totalStakeArr[i] = types.NewNodeRewardedTokens(id.Id, id.TokensRewarded, int64(block.Height))
-	}
-
-	return db.SaveNodeRewardedTokens(totalStakeArr)
-}
-
-func getNodeNetworkingKey(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating get node networking key")
-	totalStakeArr := make([]types.NodeNetworkingKey, len(nodeInfos))
-	for i, id := range nodeInfos {
-		totalStakeArr[i] = types.NewNodeNetworkingKey(id.Id, id.NetworkingKey, int64(block.Height))
-	}
-
-	return db.SaveNodeNetworkingKey(totalStakeArr)
-}
-
-func getNodeNetworkingAddress(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating get node networking address")
-	totalStakeArr := make([]types.NodeNetworkingAddress, len(nodeInfos))
-	for i, id := range nodeInfos {
-
-		totalStakeArr[i] = types.NewNodeNetworkingAddress(id.Id, id.NetworkingAddress, int64(block.Height))
-	}
-
-	return db.SaveNodeNetworkingAddress(totalStakeArr)
-}
-func getNodeInitialWeight(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating get node networking address")
-	totalStakeArr := make([]types.NodeInitialWeight, len(nodeInfos))
-	for i, id := range nodeInfos {
-		totalStakeArr[i] = types.NewNodeInitialWeight(id.Id, id.InitialWeight, int64(block.Height))
-	}
-
-	return db.SaveNodeInitialWeight(totalStakeArr)
-}
-
-func getNodeCommittedTokens(nodeInfos []types.StakerNodeInfo, block *flow.Block, db *database.Db, flowClient client.Proxy) error {
-	log.Trace().Str("module", "staking").Int64("height", int64(block.Height)).
-		Msg("updating get node networking address")
-	totalStakeArr := make([]types.NodeCommittedTokens, len(nodeInfos))
-	for i, id := range nodeInfos {
-		totalStakeArr[i] = types.NewNodeCommittedTokens(id.Id, id.TokensCommitted, int64(block.Height))
-	}
-
-	return db.SaveNodeCommittedTokens(totalStakeArr)
-}
