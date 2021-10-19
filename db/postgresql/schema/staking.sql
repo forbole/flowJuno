403ec302dd0b6fbc406868c34539895f184d4f8f
HarleyAppleChoi <appleno56@gmail.com>
2021-10-19 14:38:14 +0100

    delete unnecessary table

--- db/postgresql/schema/staking.sql
+++ db/postgresql/schema/staking.sql
@@ -35,12 +35,6 @@ CREATE TABLE current_table
   current_table TEXT NOT NULL
 );
 
-CREATE TABLE node_unstaking_tokens
-(  node_id TEXT NOT NULL ,
-  token_unstaking TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
 CREATE TABLE node_total_commitment
 (  node_id TEXT NOT NULL ,
   total_commitment TEXT NOT NULL ,
@@ -50,54 +44,6 @@ CREATE TABLE node_total_commitment
 CREATE TABLE node_total_commitment_without_delegators
 (  node_id TEXT NOT NULL ,
   total_commitment_without_delegators TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_staking_key
-(  node_id TEXT NOT NULL ,
-  node_staking_key TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_staked_tokens
-(  node_id TEXT NOT NULL ,
-  node_staked_tokens TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_role
-(  node_id TEXT NOT NULL ,
-  role TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_rewarded_tokens
-(  node_id TEXT NOT NULL ,
-  node_rewarded_tokens TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_networking_key
-(  node_id TEXT NOT NULL ,
-  networking_key TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_networking_address
-(  node_id TEXT NOT NULL ,
-  networking_address TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_initial_weight
-(  node_id TEXT NOT NULL ,
-  initial_weight TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
-CREATE TABLE node_info_from_address
-(  address TEXT NOT NULL ,
-  node_info TEXT NOT NULL ,
   height BIGINT  NOT NULL
 );
 
@@ -119,22 +65,9 @@ CREATE TABLE node_infos_from_table
   height BIGINT  NOT NULL
 );
 
-CREATE TABLE node_committed_tokens
-(  node_id TEXT NOT NULL ,
-  committed_tokens TEXT NOT NULL ,
-  height BIGINT  NOT NULL
-);
-
 CREATE TABLE cut_percentage
 (  cut_percentage BIGINT NOT NULL ,
   height BIGINT  NOT NULL
-);
-
-CREATE TABLE delegator_committed
-(  committed TEXT NOT NULL ,
-  height TEXT NOT NULL ,
-  node_id TEXT NOT NULL ,
-  delegator_id TEXT NOT NULL
 );
 
 CREATE TABLE delegator_info
@@ -153,46 +86,4 @@ CREATE TABLE delegator_info_from_address
 (  delegator_info TEXT NOT NULL ,
   height BIGINT  NOT NULL ,
   address TEXT NOT NULL
-);
-
-CREATE TABLE delegator_request
-(  request_to_unstake TEXT NOT NULL ,
-  height BIGINT  NOT NULL ,
-  node_id TEXT NOT NULL ,
-  delegator_id TEXT NOT NULL
-);
-
-CREATE TABLE delegator_rewarded
-(  rewarded TEXT NOT NULL ,
-  height BIGINT  NOT NULL ,
-  node_id TEXT NOT NULL ,
-  delegator_id TEXT NOT NULL
-);
-
-CREATE TABLE delegator_staked
-(  staked TEXT NOT NULL ,
-  height BIGINT  NOT NULL ,
-  node_id TEXT NOT NULL ,
-  delegator_id TEXT NOT NULL
-);
-
-CREATE TABLE delegator_unstaked
-(  unstaked TEXT NOT NULL ,
-  height BIGINT  NOT NULL ,
-  node_id TEXT NOT NULL ,
-  delegator_id TEXT NOT NULL
-);
-
-CREATE TABLE delegator_unstaking
-(  unstaking TEXT NOT NULL ,
-  height BIGINT  NOT NULL ,
-  node_id TEXT NOT NULL ,
-  delegator_id TEXT NOT NULL
-);
-
-CREATE TABLE delegator_unstaking_request
-(  unstaking TEXT NOT NULL ,
-  height BIGINT  NOT NULL ,
-  node_id TEXT NOT NULL ,
-  delegator_id TEXT NOT NULL
 );
