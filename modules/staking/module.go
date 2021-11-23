be80a825c88d3728bd96322142db2d88605d9f64
HarleyAppleChoi <appleno56@gmail.com>
2021-11-23 12:31:01 +0000

    delete block modules on staking module

--- modules/staking/module.go
+++ modules/staking/module.go
@@ -4,9 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/simapp/params"
 	"github.com/forbole/flowJuno/modules/messages"
 	"github.com/forbole/flowJuno/modules/modules"
-	"github.com/forbole/flowJuno/types"
 	"github.com/go-co-op/gocron"
-	"github.com/onflow/flow-go-sdk"
 
 	"github.com/forbole/flowJuno/client"
 	db "github.com/forbole/flowJuno/db/postgresql"
@@ -14,7 +12,6 @@ import (
 
 var (
 	_ modules.Module      = &Module{}
-	_ modules.BlockModule = &Module{}
 )
 
 // Module represents the x/auth module
@@ -43,17 +40,8 @@ func NewModule(
 func (m *Module) Name() string {
 	return "staking"
 }
-
-// HandleEvent implements modules.MessageModule
-func (m *Module) HandleBlock(block *flow.Block, _ *types.Txs) error {
-	return nil //HandleBlock(block, m.messagesParser, m.db, int64(block.Height), m.flowClient)
-}
-
 // RegisterPeriodicOperations implements modules.Module
 func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
-	return nil //RegisterPeriodicOps(scheduler, m.db, m.flowClient)
+	return RegisterPeriodicOps(scheduler, m.db, m.flowClient)
 }
 
-func (m *Module) HandleEvent(index int, event types.Event, tx *types.Tx) error {
-	return nil /* HandleEvent(m.messagesParser, m.db, event, tx , m.flowClient) */
-}
