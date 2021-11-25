ba97f8bede32476d741e33a93c7ee7b8820debf4
HarleyAppleChoi <appleno56@gmail.com>
2021-11-25 10:13:06 +0000

    Add config files

---
+++ types/config/parse.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	juno "github.com/forbole/flowJuno/types"
+	"github.com/pelletier/go-toml"
+)
+
+type configToml struct {
+	DatabaseConfig *DatabaseConfig `toml:"database"`
+}
+
+// ParseConfig allows to read the given file contents as a Config instance
+func ParseConfig(fileContents []byte) (juno.Config, error) {
+	// Parse the custom config
+	var cfg configToml
+	err := toml.Unmarshal(fileContents, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse Juno config
+	junoCfg, err := juno.DefaultConfigParser(fileContents)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewConfig(
+		junoCfg,
+		NewDatabaseConfig(
+			junoCfg.GetDatabaseConfig(),
+			cfg.DatabaseConfig.StoreHistoricalData,
+		),
+	), err
+}
